9914d53594855cc6fbd7f13405194d553d2612c2
ZiyuMeng98 <154886834+ZiyuMeng98@users.noreply.github.com>
2026-08-14 12:15:47 +0800

    Update main.sh

--- 漳州SARS-CoV-2_genomic_assembly/main.sh
+++ 漳州SARS-CoV-2_genomic_assembly/main.sh
@@ -2,3 +2,4 @@ ls 00.raw_data/ > samples.txt
 mkdir logs/
 bash ~/script/amplicon_genome_qc/artic_guppyplex.sh 2>&1 | tee logs/artic_guppyplex.log
 bash ~/script/amplicon_genome_qc/minimap2_samtools.sh 2>&1 | tee logs/minimap2_samtools.log
+for i in $(cat samples.txt); do SAMPLE="$i" bash ivar_trim.sh 2>&1 | tee "logs/ivar_trim_${i}.log"; done
