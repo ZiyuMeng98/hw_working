692c54ff0875f419c34366b1017d3d658873f8b1
ZiyuMeng98 <154886834+ZiyuMeng98@users.noreply.github.com>
2026-08-14 11:35:31 +0800

    Update main.sh

--- 漳州SARS-CoV-2_genomic_assembly/main.sh
+++ 漳州SARS-CoV-2_genomic_assembly/main.sh
@@ -1,1 +1,4 @@
-
+ls 00.raw_data/ > samples.txt
+mkdir logs/
+bash ~/script/amplicon_genome_qc/artic_guppyplex.sh 2>&1 | tee logs/artic_guppyplex.log
+bash ~/script/amplicon_genome_qc/minimap2_samtools.sh 2>&1 | tee logs/minimap2_samtools.log
